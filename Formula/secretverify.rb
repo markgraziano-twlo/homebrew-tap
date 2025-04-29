19c71c5cf9877587268787c12beb42611f857992
mgraziano-twlo <mgraziano@twilio.com>
2025-04-29 14:10:18 -0500

    Fix resource URLs for Python deps

--- Formula/secretverify.rb
+++ Formula/secretverify.rb
@@ -10,22 +10,22 @@ class Secretverify < Formula
   depends_on "python@3.11"
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/xx/yy/requests-2.31.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.31.0.tar.gz"
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
 
   resource "boto3" do
-    url "https://files.pythonhosted.org/packages/aa/bb/boto3-1.26.151.tar.gz"
+    url "https://files.pythonhosted.org/packages/source/b/boto3/boto3-1.26.151.tar.gz"
     sha256 "81bff32c96a6b4b203beb63826214d8cf24ca1a86e81d43bbb688a21c5d79e2a"
   end
 
   resource "google-auth" do
-    url "https://files.pythonhosted.org/packages/cc/dd/google-auth-2.23.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/source/g/google-auth/google-auth-2.23.0.tar.gz"
     sha256 "753a26312e6f1eaeec20bc6f2644a10926697da93446e1f8e24d6d32d45a922a"
   end
 
   resource "PyInquirer" do
-    url "https://files.pythonhosted.org/packages/ee/ff/PyInquirer-1.0.3.tar.gz"
+    url "https://files.pythonhosted.org/packages/source/P/PyInquirer/PyInquirer-1.0.3.tar.gz"
     sha256 "c9a92d68d7727fbd886a7908c08fd9e9773e5dc211bf5cbf836ba90d366dee51"
   end
 
