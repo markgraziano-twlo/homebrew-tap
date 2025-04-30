2671a922fb65308199564b458eaf749240367839
mgraziano-twlo <mgraziano@twilio.com>
2025-04-30 10:56:38 -0500

    secretverify v0.2.0: lean formula with Click

--- Formula/secretverify.rb
+++ Formula/secretverify.rb
@@ -4,46 +4,30 @@ class Secretverify < Formula
   desc "Validate leaked secrets from the command line"
   homepage "https://github.com/markgraziano-twlo/secretverify"
   url "https://github.com/markgraziano-twlo/secretverify/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "cf009a54ca18b7c160acf9d092a2deb0b854e799d187e1e672bc813f746fac1c"
+  sha256 "2b4191b99819b1ffc136123ab497cfe567a6ad400cca979e7a2db16c8203632b"
   license "MIT"
 
   depends_on "python@3.11"
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.31.0.tar.gz"
-    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
-  end
+  url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.31.0.tar.gz"
+  sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+end
 
-  resource "boto3" do
-    url "https://files.pythonhosted.org/packages/source/b/boto3/boto3-1.26.151.tar.gz"
-    sha256 "81bff32c96a6b4b203beb63826214d8cf24ca1a86e81d43bbb688a21c5d79e2a"
-  end
+resource "boto3" do
+  url "https://files.pythonhosted.org/packages/source/b/boto3/boto3-1.26.151.tar.gz"
+  sha256 "81bff32c96a6b4b203beb63826214d8cf24ca1a86e81d43bbb688a21c5d79e2a"
+end
 
-  resource "google-auth" do
-    url "https://files.pythonhosted.org/packages/source/g/google-auth/google-auth-2.23.0.tar.gz"
-    sha256 "753a26312e6f1eaeec20bc6f2644a10926697da93446e1f8e24d6d32d45a922a"
-  end
+resource "google-auth" do
+  url "https://files.pythonhosted.org/packages/source/g/google-auth/google-auth-2.23.0.tar.gz"
+  sha256 "753a26312e6f1eaeec20bc6f2644a10926697da93446e1f8e24d6d32d45a922a"
+end
 
-  resource "PyInquirer" do
-    url "https://files.pythonhosted.org/packages/source/P/PyInquirer/PyInquirer-1.0.3.tar.gz"
-    sha256 "c9a92d68d7727fbd886a7908c08fd9e9773e5dc211bf5cbf836ba90d366dee51"
-  end
-
-  resource "prompt_toolkit" do
-    url "https://files.pythonhosted.org/packages/source/p/prompt_toolkit/prompt_toolkit-2.0.10.tar.gz"
-    sha256 "f15af68f66e664eaa559d4ac8a928111eebd5feda0c11738b5998045224829db"
-  end
-
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/source/P/Pygments/Pygments-2.16.1.tar.gz"
-    sha256 "1daff0494820c69bc8941e407aa20f577374ee88364ee10a98fdbe0aece96e29"
-  end
-
-  resource "wcwidth" do
-    url "https://files.pythonhosted.org/packages/source/w/wcwidth/wcwidth-0.2.6.tar.gz"
-    sha256 "a5220780a404dbe3353789870978e472cfe477761f06ee55077256e509b156d0"
-  end
-
+resource "click" do
+  url "https://files.pythonhosted.org/packages/source/c/click/click-8.1.7.tar.gz"
+  sha256 "<SHA256_OF_click-8.1.7>"
+end
 
   def install
     virtualenv_install_with_resources
